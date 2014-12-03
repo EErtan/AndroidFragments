3b5149600382402375bcc24bb1934d292b838c97
ersin <ersin_the_ertan@hotmail.com>
2014-12-02 23:59:49 -0500

    Chapter 2 Read !Coded

---
+++ ch02_configchanges/src/main/java/com/nullcognition/androidfragments/DownloadImageTask.java
@@ -0,0 +1,111 @@
+package com.nullcognition.androidfragments;// .cancel(booleanValue);
+// .execute(Values);
+
+public class DownloadImageTask extends android.os.AsyncTask<String, Integer, android.graphics.Bitmap> {
+
+   private static com.nullcognition.androidfragments.DownloadImageTask downloadImageTask;
+   private        android.content.Context                              context;
+   int                     progress        = - 1;
+   android.graphics.Bitmap downloadedImage = null;
+
+   public DownloadImageTask(){
+	  super();
+   }
+
+   public static synchronized com.nullcognition.androidfragments.DownloadImageTask newInstance(android.content.Context inContext){
+	  downloadImageTask = new DownloadImageTask();
+	  downloadImageTask.setContext(inContext);
+	  return downloadImageTask;
+   }
+
+   public static com.nullcognition.androidfragments.DownloadImageTask getInstance(){
+	  return downloadImageTask;
+   }
+
+   protected void setContext(android.content.Context inContext){
+	  context = inContext;
+	  if(progress >= 0){publishProgress(progress);}
+   }
+
+   @Override
+   protected android.graphics.Bitmap doInBackground(String... inURLS){
+	  return DownloadImage(inURLS);
+   }
+
+   public android.graphics.Bitmap DownloadImage(String... inURLS){
+	  org.apache.http.client.HttpClient httpClient = CustomHttpClient.getHttpClient();
+	  try{
+		 org.apache.http.client.methods.HttpGet request = new org.apache.http.client.methods.HttpGet(urls[0]);
+		 org.apache.http.params.HttpParams params = new org.apache.http.params.BasicHttpParams();
+		 org.apache.http.params.HttpConnectionParams.setSoTimeout(params, 60000);   // 1 minute
+		 request.setParams(params);
+
+		 setProgress(25);
+
+		 org.apache.http.HttpResponse response = httpClient.execute(request);
+
+		 setProgress(50);
+
+		 sleepFor(5000);    // five second sleep
+
+		 byte[] image = org.apache.http.util.EntityUtils.toByteArray(response.getEntity());
+
+		 setProgress(75);
+
+		 android.graphics.Bitmap mBitmap = android.graphics.BitmapFactory.decodeByteArray(image, 0, image.length);
+
+		 setProgress(100);
+
+		 return mBitmap;
+	  }
+	  catch(java.io.IOException e){
+		 // covers:
+		 //      ClientProtocolException
+		 //      ConnectTimeoutException
+		 //      ConnectionPoolTimeoutException
+		 //      SocketTimeoutException
+		 e.printStackTrace();
+	  }
+	  return null;
+   }
+
+   private void setProgress(int progress) {
+	  this.progress = progress;
+	  publishProgress(this.progress);
+   }
+
+   protected void setImageInView() {
+	  if(downloadedImage != null) {
+		 android.widget.ImageView mImage = (android.widget.ImageView)
+		   ((android.app.Activity) mContext).findViewById(com.nullcognition.ch02_configchanges.R.id.image);
+		 mImage.setImageBitmap(downloadedImage);
+	  }
+   }
+
+
+   @Override
+   protected void onPreExecute(){
+	  super.onPreExecute();
+	  progress = 0;
+   }
+
+   @Override
+   protected void onPostExecute(android.graphics.Bitmap inResult){
+	  super.onPostExecute(inResult);
+   }
+
+   @Override
+   protected void onProgressUpdate(Integer... inProgress){
+	  super.onProgressUpdate(inProgress);
+   }
+
+   @Override
+   protected void onCancelled(android.graphics.Bitmap inResult){
+	  super.onCancelled(inResult);
+   }
+
+   @Override
+   protected void onCancelled(){
+	  super.onCancelled();
+   }
+}
--- ch02_configchanges/src/main/java/com/nullcognition/androidfragments/MainActivity.java
+++ ch02_configchanges/src/main/java/com/nullcognition/androidfragments/MainActivity.java
@@ -8,11 +8,43 @@ import android.view.MenuItem;
 import com.nullcognition.ch02_configchanges.R;
 
 public class MainActivity extends Activity {
+   // as a rule image downloading should not be done in an async task as the network may be of poor quality,
+   // thus one should use a service for the persistance throughout the application lifecyle.
+
+   // asynctask is used for demo purposes to showcase the persistant task across lifecycles
+
+   private DownloadImageTask diTask;
 
    @Override
    protected void onCreate(Bundle savedInstanceState){
 	  super.onCreate(savedInstanceState);
 	  setContentView(R.layout.activity_main);
+
+	  if((diTask = DownloaImageTask.getInstance()) != null){
+
+		 diTask.setContext(this);
+		 if(ddiTask.getStatus() == android.os.AsyncTask.Status.FINISHED){
+			diTask.setImageInView();
+		 }
+	  }
+
+   }
+
+   String httpResourceLocation = "https://uetnhht.notheuhn.com/tnoueh/snotuh/snuoth";
+
+   public void doClick(android.view.View inView){
+
+	  if(diTask != null){
+		 android.os.AsyncTask.Status diStatus = diTask.getStatus();
+		 android.util.Log.e(getClass().getSimpleName(), "ditask status = " + diStatus);
+
+		 if(diStatus != android.os.AsyncTask.Status.FINISHED){
+			android.util.Log.e(getClass().getSimpleName(), "finished the task");
+			return;
+		 }
+	  }
+	  diTask = DownloadImageTask.newInstance(this);
+	  diTask.execute(httpResourceLocation);
    }
 
 
